dfd41e19f3642c57128725589408501658391fd1
AlessandroCarreno159 <alessandro_carreno@usmp.pe>
2026-09-25 17:58:11 -0500

    feat(1): Implementar Curso/Matricula + seed en Home - Implementar Curso/Matricula + seed en Home + cambio de interfaz

--- obj/Debug/net10.0/portal-academico.AssemblyInfo.cs
+++ obj/Debug/net10.0/portal-academico.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("portal-academico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+048021a73895d6f55134f95c6c4a2a41e10cd2f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2687bfd79f8c5ce2555cfa9fe280ef35b2a36770")]
 [assembly: System.Reflection.AssemblyProductAttribute("portal-academico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("portal-academico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
